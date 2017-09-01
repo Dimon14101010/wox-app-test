689724e4706fb54f041226e4449f805fd112705f
Dimon14101010 <dmitriy.nekrasov89>
2017-09-01 18:49:18 +0300

    filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,14 +10,17 @@ import {SearchPipe} from "../pipes/search.pipe";
 export class AppComponent implements OnInit {
   itemsOnPage = 50;
   pages = [];
+  headerdata: any = [];
   data: any = [];
   constructor (private http: HttpClient) {
 
   }
   ngOnInit () {
       this.http.get('./src/data/data-source.json')
-        .subscribe(response => {
-          this.data = response;
+        .subscribe((response: Array<any>) => {
+          this.data = response.slice(1, response.length);
+          this.headerdata = response[0];
+          console.log(this.headerdata, this.data);
           this.pages = new Array(Math.ceil(this.data.length / this.itemsOnPage));
         });
 
