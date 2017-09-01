d9a28a6149ea566fdec998994835b22485c7ea20
Dimon14101010 <dmitriy.nekrasov89>
2017-09-01 22:07:17 +0300

    filter

--- src/pipes/search.pipe.ts
+++ src/pipes/search.pipe.ts
@@ -1,13 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'search' })
 export class SearchPipe implements PipeTransform {
-  transform(items: any, searchText: any): any {
-    if (searchText == null) {return items; }
-
-    return items.filter(function(row){
-      console.log('row', row);
-      return row.toLowerCase().indexOf(searchText.toLowerCase()) > -1;
-    });
+  transform(value: Array<any>, searchText: string, row: Array<any>): any {
+    if (!searchText) {
+      return value;
+    } else {
+      return value.filter(function (insideArr: Array<any>) {
+        return insideArr.indexOf(searchText) > -1;
+       });
+    }
   }
-}
 
+}
